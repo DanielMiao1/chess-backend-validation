b8aba41bde147580c9feb88957078c9053d03491
Daniel Miao <danielmiao2019@icloud.com>
2022-09-02 14:25:12 -0700

    added post handler for /game

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ app.listen(34874, function() {
 app.get("/", function(request, response) {
   response.redirect("https://multiplayer-chess.gq/");
 });
+
+app.post("/game", function(request, response) {
+  response.setHeader("Content-Security-Policy", "default-src *");
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.set("Cache-Control", "no-store");
+  console.log(request.body);
+});
