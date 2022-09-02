2be86e7cb5b7b41148fa607650370aa1635fe2d0
Daniel Miao <danielmiao2019@icloud.com>
2022-09-02 13:41:52 -0700

    set up index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(34874, function() {
+  console.log("Started listening for requests on port 34874");
+});
+
+app.get("/", function(request, response) {
+  response.redirect("https://multiplayer-chess.gq/");
+});
