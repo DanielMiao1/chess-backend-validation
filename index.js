31a0565d2c952def13db698b5ebfd4ac07e50234
Daniel Miao <danielmiao2019@icloud.com>
2022-09-05 16:28:26 -0700

    implemented functions for new games and requests for game states

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require("express");
+import Express from "express";
+import bodyParser from "body-parser";
+import { Chess } from "chess.js";
 
-const app = express();
+const app = Express();
+
+var games = {};
+var game_index = 0;
+
+app.use(bodyParser.text());
 
 app.listen(34874, function() {
   console.log("Started listening for requests on port 34874");
@@ -11,8 +18,22 @@ app.get("/", function(request, response) {
 });
 
 app.post("/game", function(request, response) {
-  response.setHeader("Content-Security-Policy", "default-src *");
   response.setHeader("Access-Control-Allow-Origin", "*");
-  response.set("Cache-Control", "no-store");
-  console.log(request.body);
+  game_index++;
+  games[game_index] = {"id": game_index, "type": JSON.parse(request.body)["type"], board: new Chess()};
+  response.send("0".repeat(5 - game_index.toString().length) + game_index.toString());
 });
+
+app.get("/moves/:id([0-9]{5})", function(request, response) {
+  response.send("")
+});
+
+app.get("/turn/:id([0-9]{5})", function(request, response) {
+  console.log(id);
+  // response.send(games[id])
+});
+
+app.get("/state/:id([0-9]{5})", function(request, response) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.send(JSON.stringify(games[parseInt(request.params.id)].board.board()))
+})
