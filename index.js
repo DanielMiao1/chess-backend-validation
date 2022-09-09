d302ef66f1e16715e66502677b0dc5e4941fe71d
Daniel Miao <danielmiao2019@icloud.com>
2022-09-09 15:05:35 -0700

    fixed imports

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import { Server } from "http";
 import Express from "express";
 import bodyParser from "body-parser";
 import { Chess } from "./chess.js/chess.js";
-import { Server } from "./socket.io/socket.js";
+import * as SocketIO from "socket.io";
 
 const app = Express();
 
@@ -10,7 +11,15 @@ var game_index = 0;
 
 app.use(bodyParser.text());
 
-app.listen(34874, function() {
+const http_server = Server(app);
+
+const socket = new SocketIO.Server(http_server, {
+  cors: {
+    origin: ["https://www.multiplayer-chess.gq", "http://localhost:8000/"]
+  }
+});
+
+http_server.listen(34874, function() {
   console.log("Started listening for requests on port 34874");
 });
 
@@ -25,16 +34,13 @@ app.post("/game", function(request, response) {
   response.send("0".repeat(5 - game_index.toString().length) + game_index.toString());
 });
 
-app.get("/moves/:id([0-9]{5})", function(request, response) {
-  response.send("")
-});
-
-app.get("/turn/:id([0-9]{5})", function(request, response) {
-  console.log(id);
-  // response.send(games[id])
-});
-
 app.get("/state/:id([0-9]{5})", function(request, response) {
   response.setHeader("Access-Control-Allow-Origin", "*");
   response.send(JSON.stringify(games[parseInt(request.params.id)].board.board()))
-})
+});
+
+// const http_server = http.createServer(app).listen(app.get("port"), function() { });
+
+socket.on("connection", function(socket) {
+  console.log(socket);
+});
