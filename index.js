c84d32534d45306addbefea42ded93b4c780ffda
Daniel Miao <danielmiao2019@icloud.com>
2022-09-09 16:06:33 -0700

    added socket handler

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const http_server = Server(app);
 
 const socket = new SocketIO.Server(http_server, {
   cors: {
-    origin: ["https://www.multiplayer-chess.gq", "http://localhost:8000/"]
+    origin: "https://www.multiplayer-chess.gq"
   }
 });
 
@@ -34,13 +34,12 @@ app.post("/game", function(request, response) {
   response.send("0".repeat(5 - game_index.toString().length) + game_index.toString());
 });
 
-app.get("/state/:id([0-9]{5})", function(request, response) {
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.send(JSON.stringify(games[parseInt(request.params.id)].board.board()))
-});
-
-// const http_server = http.createServer(app).listen(app.get("port"), function() { });
-
-socket.on("connection", function(socket) {
-  console.log(socket);
-});
+// socket.on("connection", function(socket) {
+//   socket.on("state", function(game_id) {
+//     if (game_id.length != 5 || parseInt(game_id) > game_index) {
+//       return;
+//     };
+//     socket.emit("state", JSON.stringify(games[parseInt(game_id)].board.board()))
+//     socket.emit("type", JSON.stringify(games[parseInt(game_id)].type))
+//   });
+// });
